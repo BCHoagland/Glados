9d0af59ba4f6e59e934a453179e0a07e68ae99a4
Braden Hoagland <bhoggland@gmail.com>
2019-08-01 15:27:32 -0400

    hopefully works

---
+++ new/model.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+
+from visualize import box
+from utils import get_device
+
+class RNN(nn.Module):
+    def __init__(self, n_chars, n_hidden=256, n_layers=2, drop_prob=0.5):
+        super().__init__()
+        
+        # params
+        self.n_layers = n_layers
+        self.n_hidden = n_hidden
+        self.device = get_device()
+
+        # network layers
+        self.embed = nn.Embedding(n_chars, n_hidden)
+        self.lstm = nn.LSTM(n_hidden, n_hidden, n_layers, dropout=drop_prob, batch_first=True)
+        self.dropout = nn.Dropout(drop_prob)
+        self.linear = nn.Linear(n_hidden, n_chars)
+
+        # box('Network Architecture')
+        # print(self)
+
+
+    def forward(self, x, hidden):
+        x = self.embed(x)
+        out, hidden = self.lstm(x, hidden)
+        out = self.dropout(out)
+        out = self.linear(out)
+        return out, hidden
+
+
+    def blank_hidden(self, batch_size=1):
+        return (torch.zeros(self.n_layers, batch_size, self.n_hidden).to(self.device),
+                torch.zeros(self.n_layers, batch_size, self.n_hidden).to(self.device))
---
+++ new/train.py
@@ -0,0 +1,74 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from model import RNN
+from utils import read_data, batches, get_device
+from visualize import progress, plot
+
+
+batch_size = 128
+seq_size = 100
+
+X, Y, n_chars, char2int, int2char, num_batches = read_data('vonnegut', batch_size, seq_size)
+
+net = RNN(n_chars).to(get_device())
+opt = torch.optim.Adam(net.parameters(), lr=0.001)
+
+'''
+TRAINING
+'''
+def train(epochs=20):
+    for epoch in range(epochs):
+
+        batch_num = 0
+        h = net.blank_hidden(batch_size)
+        for x, y in batches(X, Y, batch_size, seq_size):
+            out, h = net(x, h)
+            h = tuple([state.detach() for state in h])
+
+            loss = F.cross_entropy(out.transpose(1,2), y)
+
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+            plot(num_batches * epoch + batch_num, loss, 'Loss', 'Training', '#FA5784')
+            progress(batch_num, num_batches, num_batches * epoch + batch_num, epochs * num_batches)
+
+            batch_num += 1
+
+
+'''
+GENERATION
+'''
+def next_char(x):
+    probs, indices = torch.topk(x, k=5)
+    probs, indices = probs.squeeze().tolist(), indices.squeeze().tolist()
+    probs = [p / sum(probs) for p in probs]
+    choice = int2char[np.random.choice(indices, p=probs)]
+    return choice
+
+def generate():
+    with torch.no_grad():
+        first_chars = list('I will')
+        chars = ''.join(first_chars)
+
+        # run initial chars through model to generate hidden states
+        h = net.blank_hidden()
+        for c in first_chars:
+            x, h = net(torch.tensor([[char2int[c]]]), h)
+
+        # generate new chars
+        for _ in range(100):
+            choice = next_char(x)
+            chars += choice
+            x, h = net(torch.tensor([[char2int[choice]]]), h)
+
+        print(chars)
+
+'''
+"DO IT" - Palpatine
+'''
+train()
+generate()
